ba250b2fc434569256b1b961f23dc73bfe237f8e
CristinaSoy <ccardonaaltes@hotmail.com>
2024-12-17 18:21:19 +0100

    Versio revisada. Invocar la funció només 1 cop

--- sprint1_tema2_nivel2.php
+++ sprint1_tema2_nivel2.php
@@ -17,6 +17,7 @@ function calculaCost($duracio) : float {
 }
 
 echo "El cost d'una trucada de $duracio minuts és de ". calculaCost($duracio). "€.";
+
 echo"<h3>Exercici 2. Afegeix subtotals al carret de la compra.</h3>";
 
 $tarifari = [
@@ -24,18 +25,23 @@ $tarifari = [
     "xiclets"=> 0.50,
     "caramels"=> 1.5,
 ];
-$total = 0.0;
 
-function afegirCompra($tarifari, $producte, $unitats, &$total): void {
- $subtotal= $tarifari[$producte] * $unitats;
- $total += $subtotal;
- echo "S'ha afegit la quantitat de $unitats $producte. Subtotal: $subtotal €. Total: $total € <br>";  
+
+function calculaSubtotal($producte, $unitats): float {
+    global $tarifari;
+    $subtotal= $tarifari[$producte] * $unitats;
+    echo "$unitats $producte Subtotal: $subtotal €.<br>";  
+    return $subtotal;
 }
 
-afegirCompra($tarifari, "xocolates", 2, $total);
-afegirCompra($tarifari, "xiclets", 1, $total);
-afegirCompra($tarifari,"caramels", 1, $total);
+function calculaTotal() : float {
+    $total = calculaSubtotal("xocolates", 2)+
+            calculaSubtotal("xiclets", 1)+
+            calculaSubtotal("caramels", 1);
+    return $total;
+}
 
+$total = calculaTotal();
 echo "Total final $total €.";
 
 ?>
